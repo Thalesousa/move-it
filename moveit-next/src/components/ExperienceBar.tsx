7b242e9215a0fc4fb862d0ee806737a1910b4c88
thalesousa <thalestjsb@gmail.com>
2021-02-24 21:05:54 -0300

    Aula 03

--- moveit-next/src/components/ExperienceBar.tsx
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -1,16 +1,21 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
-	return (
-		<header className={styles.experienceBar}>
-			<span>0 xp</span>
-			<div>
-				<div style={{ width: '50%' }} />
-				<span className={styles.currentExperience} style={{ left: '50%' }}>
-					300 xp
+  const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+  const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: `${percentToNextLevel}%` }} />
+        <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>
+          {currentExperience} xp
         </span>
-			</div>
-			<span>600 px</span>
-		</header>
-	);
+      </div>
+      <span>{experienceToNextLevel} px</span>
+    </header>
+  );
 }
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from '../styles/pages/Home.module.css';
 import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -19,7 +20,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
